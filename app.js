1ea224f2ade10cc3b875d9e59f861d8cadddb3b9
Karlis <curtislv@gmail.com>
2022-05-14 17:15:17 +0300

    14.05.2022. 17.15
     - Added main logic for creating x by x square
     - Added color change on hover
     - Overall refactor

--- app.js
+++ app.js
@@ -5,12 +5,41 @@ row.classList.add('row');
 let square = document.createElement('div');
 square.classList.add('square');
 
-for (items = 1; items <= 4; items++) {
-    row.appendChild(square.cloneNode(true));
-}
+const createSquares = (size) => {
+    //
+    for (items = 1; items <= size; items++) {
+        row.appendChild(square.cloneNode(true));
+    }
 
-console.log(row);
+    for (rows = 1; rows <= size; rows++) {
+        container.appendChild(row.cloneNode(true));
+    }
+};
 
-for (rows = 1; rows <= 4; rows++) {
-    container.appendChild(row.cloneNode(true));
-}
+createSquares(4);
+
+container.addEventListener('mouseover', (event) => {
+    if (event.target.classList.contains('square')) {
+        event.target.classList.add('active');
+    }
+});
+
+const promptSquareSize = () => {
+    let gridSize;
+    do {
+        gridSize = prompt('Enter a number please?', 0);
+    } while (!isFinite(gridSize));
+
+    if (gridSize > 10) {
+        gridSize = 10;
+    }
+
+    container.innerHTML = '';
+    row.innerHTML = '';
+
+    createSquares(gridSize);
+};
+
+const button = document.querySelector('.button-85');
+
+button.addEventListener('mouseup', promptSquareSize);
